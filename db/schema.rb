92f2cf87ef144769c5538a670339269b2d79a882
Bryan0066 <bgenest96@gmail.com>
2022-08-05 10:47:12 -0400

    added admin functionality for users and restricted the creation of beaches to admins

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_03_194438) do
+ActiveRecord::Schema.define(version: 2022_08_05_134750) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,18 +22,6 @@ ActiveRecord::Schema.define(version: 2022_08_03_194438) do
     t.text "description", null: false
     t.text "url"
     t.text "image"
-  end
-
-  create_table "reviews", force: :cascade do |t|
-    t.string "title", null: false
-    t.integer "rating", null: false
-    t.string "text", null: false
-    t.bigint "users_id"
-    t.bigint "beach_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["beach_id"], name: "index_reviews_on_beach_id"
-    t.index ["users_id"], name: "index_reviews_on_users_id"
   end
 
   create_table "users", force: :cascade do |t|
