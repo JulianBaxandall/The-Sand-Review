ddb6ead50af40d35d4bf27d716a0e21c27de9d2c
scottnlesser <scottnl93@gmail.com>
2022-08-10 16:48:44 -0400

    Committing work for end of day

--- app/controllers/api/v1/votes_controller.rb
+++ app/controllers/api/v1/votes_controller.rb
@@ -3,12 +3,21 @@ class Api::V1::VotesController < ApplicationController
 
     def create 
         # check whether there exists a vote with the same user_id and review_id
-        vote = Vote.new(vote_params)
-        vote.user_id = current_user.id
-        if vote.save
-            render json: vote
+
+        if Vote.find_by(user_id: current_user.id , review_id: vote_params["review_id"])
+            previousValue = Vote.find_by(user_id: current_user.id , review_id: vote_params["review_id"]).value
+            updatedValue = params["value"]
+            Vote.find_by(user_id: current_user.id , review_id: vote_params["review_id"]).value = updatedValue
+            difference = updatedValue - previousValue
+            render json: difference
         else
-            render json: {error: review.errors.full_messages}, status: :unprocessable_entity
+            vote = Vote.new(vote_params)
+            vote.user_id = current_user.id
+            if vote.save
+                render json: vote.value
+            else
+                render json: {error: review.errors.full_messages}, status: :unprocessable_entity
+            end
         end
     end
 
--- app/javascript/react/components/VotingComponent.js
+++ app/javascript/react/components/VotingComponent.js
@@ -3,12 +3,16 @@ import VoteCountComponent from "./VoteCountComponent"
 
 const VotingComponent = (props) => {
     const[count, setCount] = useState(props.netCount)
-    const submitUpVote = async (event) => {
+    const submitVote = async (event) => {
         event.preventDefault
+        let voteValue = 1
+        if (event.currentTarget.value === "down"){
+            voteValue = -1
+        }
         try {
             const beach_id = props.beach_id       
             const review_id = props.review_id
-            let formPayload = {"vote": {"value":1, "review_id":review_id}}
+            let formPayload = {"vote": {"value":voteValue, "review_id":review_id}}
             const response = await fetch(`/api/v1/beaches/${beach_id}/votes`, {
                 credentials: "same-origin",
                 method: "POST",
@@ -19,38 +23,19 @@ const VotingComponent = (props) => {
                 body: JSON.stringify(formPayload),
               })
             const voteData = await response.json()
-            setCount(count + 1)
+            setCount(count + voteData)
         } catch(error){
             console.log("error in fetch:", error);
         }
     }
 
-    const submitDownVote = async (event) => {
-        event.preventDefault
-        try {
-            const beach_id = props.beach_id       
-            const review_id = props.review_id
-            let formPayload = {"vote": {"value":-1, "review_id":review_id}}
-            const response = await fetch(`/api/v1/beaches/${beach_id}/votes`, {
-                credentials: "same-origin",
-                method: "POST",
-                headers: {
-                  Accept: "application/json",
-                  "Content-Type": "application/json",
-                },
-                body: JSON.stringify(formPayload),
-              })
-            const voteData = await response.json()
-        } catch(error){
-            console.log("error in fetch:", error);
-        }
-    }
+
 
     return(
         <div>
             <form>
-                <input type = "button" value="up" onClick = {submitUpVote}></input>
-                <input type = "button" value="down" onClick = {submitDownVote}></input>
+                <input type = "button" value="up" onClick = {submitVote}></input>
+                <input type = "button" value="down" onClick = {submitVote}></input>
             </form>
             <div>
                 <h5>Current Count:</h5>
